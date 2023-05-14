ffb27c18546ebd9be65b6b74a031c807cba2427e
Namitha G S <99288233+Namitha-GS@users.noreply.github.com>
2023-05-14 16:24:55 +0530

    Update Minimum_no_of_jumps_to_reach_end_of_array.java

--- Minimum_no_of_jumps_to_reach_end_of_array.java
+++ Minimum_no_of_jumps_to_reach_end_of_array.java
@@ -2,10 +2,11 @@ package data_structures;
 
 public class Minimum_no_of_jumps_to_reach_end_of_array {
 	
-	private static int minJump(int[] a, int n) {
-		int step = a[0];
+     static int minJumps(int[] a){
+                int step = a[0];
 		int maxReach = a[0];
 		int jump = 1;
+		int n = a.length;
 		
 		if(n <= 1)
 			return 0;
@@ -16,19 +17,23 @@ public class Minimum_no_of_jumps_to_reach_end_of_array {
 		// since 0th index is already considered, 
 		// start iterating from 1st index
 		for(int i=1; i<n; i++) {
-			maxReach = Math.max(maxReach, a[i]+i);
-			step--;
-			if(step == 0) {
-				if(maxReach-i <= 0) {
-					// to check again is step == 0
-					return -1;
-				}
-				step = maxReach-i;
-				jump++;
+		    // if i reaches end of array then don't increment jump, just directly return jump
+		    if(i == a.length-1){
+		        return jump;
+		    }
+		     maxReach = Math.max(maxReach, a[i]+i);
+		     step--;
+		     if(step == 0) {
+			if(maxReach-i <= 0) {
+				// to check again is step == 0
+				return -1;
 			}
+			step = maxReach-i;
+			jump++;
+		    }
 		}
-		return jump;
-	}
+		return -1;
+    }
 
 	public static void main(String[] args) {
 		int[] arr = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
